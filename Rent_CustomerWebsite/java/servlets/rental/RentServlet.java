8d2a352062fb53f1def2aa2b915c3c5d8171e1b7
Tianyi Bao <tianyib831@gmail.com>
2022-04-27 17:27:50 -0400

    Added comments explaining the code

--- Rent_CustomerWebsite/java/servlets/rental/RentServlet.java
+++ Rent_CustomerWebsite/java/servlets/rental/RentServlet.java
@@ -28,6 +28,8 @@ public class RentServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		// Retriving user input from website
 		String name = request.getParameter("Name");
 		String address = request.getParameter("Address");
 		Long phone = Long.parseLong(request.getParameter("Phone"));
@@ -45,30 +47,38 @@ public class RentServlet extends HttpServlet {
 		System.out.println("---------END----------");
 		
 		try {
+			// Establish connection with database
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/best_rent", "root", "bao1tian2");
+			// CHANGE YOUR USER NAME AND PASSWORD HERE
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/best_rent", "root", "password");
 			Statement statement = connection.createStatement();
 			
+			// Total rental price calculation based on number of days of rental
 			LocalDate dateBefore = LocalDate.parse(pickUp);
 			LocalDate dateAfter = LocalDate.parse(due);
-			//calculating number of days in between
+			// Calculating number of days in between
 			int noOfDaysBetween = (int) ChronoUnit.DAYS.between(dateBefore, dateAfter);
 			System.out.println("Days Between: " + noOfDaysBetween);
+			// Query for retriving the selected vehicle's price per day.
 			ResultSet car_price_result = statement.executeQuery("select price from vehicle where vehicle_num = '"+vehicleID+"'");
 			car_price_result.next();
 			System.out.println("Car Price: " + car_price_result.getInt(1));
+			// Total Price 
 			int totalPrice = car_price_result.getInt(1) * noOfDaysBetween;
 			System.out.println("Total Price: " + totalPrice);
 			
-			
+			// Add new customer profile to the database
 			statement.executeUpdate("insert into customer (name, credits, address, mobile) values ('"+name+"', '5', '"+address+"', '"+phone+"')");
 			
+			// Get customer id
 			ResultSet customer_result = statement.executeQuery("select id from customer where name = '"+name+"'");
 			customer_result.next();
 			int customer_id = customer_result.getInt(1);
 			System.out.println("Customer ID: " + customer_id);
 			
+			// Insert customer, total price, vehicle associated with to database
 			statement.executeUpdate("insert into rental (car_id, cust_id, fee, date, due) values ('"+vehicleID+"', '"+customer_id+"', '"+totalPrice+"', '"+pickUp+"', '"+due+"')");
+			// Return message to user
 			PrintWriter out = response.getWriter();
 			out.print("<meta http-equiv = 'refresh' content = '5; url = index.jsp' />");
 			out.print("<H1 style='text-align:center'>Rental request submitted.</H1>");
